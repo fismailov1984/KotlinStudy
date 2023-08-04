67e29a906454259c1a70b9539e4f98c6199c77a3
FIsmailov <farhodbjj1984@gmail.com>
2023-08-04 18:50:31 +0500

    first light data

---
+++ src/main/kotlin/lesson1/first_light_data.kt
@@ -0,0 +1,18 @@
+package lesson1
+
+fun main(){
+
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("The distance that Gagarin flew while he was in space in millimeters: $length")
+    println("Gagarin is age at the time of the flight: $age")
+    println("part of the day Gagarin spent in space: $partOfDay")
+    println("number of seconds Gagarin spent in space: $seconds")
+    println("part of the year that Gagarin spent in space: $partOfYear")
+    println("apogee of Gagarin is orbit in meters: $apogee")
+}
