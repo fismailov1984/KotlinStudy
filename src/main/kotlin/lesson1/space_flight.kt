e3723434dad7327862ab8a84cd60508234f2ef61
FIsmailov <farhodbjj1984@gmail.com>
2023-08-04 18:46:45 +0500

    space flight

---
+++ src/main/kotlin/lesson1/space_flight.kt
@@ -0,0 +1,20 @@
+package lesson1
+
+fun main(){
+
+    val yearSpaceFlight: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+
+    println(yearSpaceFlight)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print(hour)
+    print(":")
+    print(minute)
+
+}
