ff25e42e3d4ac621cec8f43a3e0dc13b58885260
FIsmailov <farhodbjj1984@gmail.com>
2023-08-06 11:15:33 +0500

    welcome function

---
+++ src/main/kotlin/FIO_age.kt
@@ -0,0 +1,13 @@
+fun main(){
+
+    val lastName: String = "Андреева"
+    val maidenName: String = "Татьяна"
+    val surname: String = "Сергеевна"
+    val firstName: String = "Сидорова"
+    val age1: Int = 20
+    val age2: Int = 22
+
+    println("FIO: $lastName $maidenName $surname, Age: $age1")
+    println("FIO: $lastName $firstName $surname, Age: $age2")
+
+}
