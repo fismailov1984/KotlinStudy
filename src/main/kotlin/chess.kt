219c091625cff4c85f97d653eab0d7e6e057c96d
FIsmailov <farhodbjj1984@gmail.com>
2023-08-06 21:33:29 +0500

    correct print

--- src/main/kotlin/chess.kt
+++ src/main/kotlin/chess.kt
@@ -2,25 +2,17 @@ import java.awt.List
 
 fun main(){
 
-    val number = 1
+    var number: Int = 1
     var startPoint: String = "E2"
     var endPoint: String = "E4"
-    var table: String = "$startPoint-$endPoint; ${number}"
+    var table: String = "$startPoint-$endPoint;${number}"
     println(table)
 
     startPoint = "D2"
     val step = 1
-    endPoint = "${startPoint.first()}${(startPoint.last() + number)}"
+    endPoint = "${startPoint.first()}${(startPoint.last() + step)}"
 
-    table = "$startPoint-$endPoint; ${number+step}"
+    table = "$startPoint-$endPoint;${++number}"
     println(table)
-
-
-
-
-
-
-
-
 
 }
