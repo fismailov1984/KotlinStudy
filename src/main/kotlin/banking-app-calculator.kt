83da28ece299366160e01d882f7a1b767c87b481
FIsmailov <farhodbjj1984@gmail.com>
2023-08-06 11:28:36 +0500

    delete comment

--- src/main/kotlin/banking-app-calculator.kt
+++ src/main/kotlin/banking-app-calculator.kt
@@ -2,9 +2,6 @@ import kotlin.math.pow
 
 fun main(){
 
-//    Сумму вклада можно вводить с клавиатуры таким образом:
-//    val depositAmount: Double = readln().toDouble()
-
     val depositAmount: Double = 70_000.0
     val annualInterestRate: Double = 16.7
     val depositTerm = 20
