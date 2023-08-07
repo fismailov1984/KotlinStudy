3d938c0a856e98dbba46e4320d0a69e709beb912
FIsmailov <farhodbjj1984@gmail.com>
2023-08-07 09:13:40 +0500

    cargo check

---
+++ src/main/kotlin/custom_terminal.kt
@@ -0,0 +1,18 @@
+fun main(){
+
+val cargoVolume = arrayOf(80, 100)
+val cargoWeight = arrayOf(20, 50)
+
+    cargoCheck(cargoVolume[0], cargoWeight[0])
+    cargoCheck(cargoVolume[1], cargoWeight[1])
+
+}
+fun cargoCheck(volume: Int, weight: Int){
+
+    val volumeMax: Int = 100
+    val weightMax: Int = 100
+
+    println("A load of $weight and a volume of $volume liters corresponds to the category 'Average'" +
+            ":${volume < volumeMax && weight <= weightMax}")
+
+}
