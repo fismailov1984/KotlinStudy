b4b393c8e8ec67f4d549773d03961440f4b3ff16
FIsmailov <farhodbjj1984@gmail.com>
2023-08-07 11:54:44 +0500

    monitoring system

---
+++ src/main/kotlin/training_program.kt
@@ -0,0 +1,16 @@
+fun main(){
+
+    val day: Int = 5
+    val exercise = "exercises of the"
+    val mapMutableList = mutableMapOf(
+        "arms" to 1,
+        "legs" to 2,
+        "back" to 2,
+        "press" to 1,
+    )
+
+    for(i in mapMutableList){
+            println("$exercise ${i.key}: ${(day % 2 == 1)&&(i.value == 1)||(day % 2 == 0)&&(i.value == 2)}")
+        }
+    }
+
