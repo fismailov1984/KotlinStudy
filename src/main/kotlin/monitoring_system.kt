b5a3016cba0e2c460a0e40c107d5a0943edb9f84
FIsmailov <farhodbjj1984@gmail.com>
2023-08-08 20:47:27 +0500

    added constants

--- src/main/kotlin/monitoring_system.kt
+++ src/main/kotlin/monitoring_system.kt
@@ -1,14 +1,13 @@
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
+const val IS_AIR_HUMIDITY = 20
+const val IS_SEASON = "winter"
 fun main() {
 
-    val sunnyWeather: Boolean = true
-    val openTent: Boolean = true
-    val airHumidity: Int = 20
-    val season: String = "winter"
-
-    conditionCheck(sunnyWeather, openTent, airHumidity, season)
+    checkCondition(IS_SUNNY, IS_AWNING_OPEN, IS_AIR_HUMIDITY, IS_SEASON)
 }
 
-fun conditionCheck(weather: Boolean, tent: Boolean, humidity: Int, season: String) {
+fun checkCondition(weather: Boolean, tent: Boolean, humidity: Int, season: String) {
 
     println(
         "Are the conditions right now for the growth of legumes? " +
