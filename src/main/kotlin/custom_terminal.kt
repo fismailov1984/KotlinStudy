a471cea1924b693018cd8f84a004e9fae7bd8b05
FIsmailov <farhodbjj1984@gmail.com>
2023-08-08 20:31:33 +0500

    added constants

--- src/main/kotlin/custom_terminal.kt
+++ src/main/kotlin/custom_terminal.kt
@@ -1,21 +1,20 @@
+const val VOLUME_MAX = 100
+const val WEIGHT_MAX = 100
 fun main() {
 
     val cargoVolume = arrayOf(80, 100)
     val cargoWeight = arrayOf(20, 50)
 
-    cargoCheck(cargoVolume[0], cargoWeight[0])
-    cargoCheck(cargoVolume[1], cargoWeight[1])
+    checkCargo(cargoVolume[0], cargoWeight[0])
+    checkCargo(cargoVolume[1], cargoWeight[1])
 
 }
 
-fun cargoCheck(volume: Int, weight: Int) {
-
-    val volumeMax: Int = 100
-    val weightMax: Int = 100
+fun checkCargo(volume: Int, weight: Int) {
 
     println(
         "A load of $weight and a volume of $volume liters corresponds to the category 'Average'" +
-                ":${volume < volumeMax && weight <= weightMax}"
+                ":${volume < VOLUME_MAX && weight <= WEIGHT_MAX}"
     )
 
 }
