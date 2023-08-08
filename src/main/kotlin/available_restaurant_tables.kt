3d2bd3df5c6bb8234f6ecd9dcb560709ebd7da19
FIsmailov <farhodbjj1984@gmail.com>
2023-08-08 10:36:57 +0500

    delete replace, 2 arguments in split

--- src/main/kotlin/available_restaurant_tables.kt
+++ src/main/kotlin/available_restaurant_tables.kt
@@ -1,19 +1,21 @@
+const val ALL_TABLES: Int = 13
 fun main() {
 
-    val allTables: Int = 13
-    val availableTablesToday = 13
-    val availableTablesTomorrow = 4
+    val occupiedTablesToday: Int = ALL_TABLES
+    val availableTablesTomorrow: Int = 4
+    val occupiedTablesTomorrow: Int = ALL_TABLES - availableTablesTomorrow
     val textToday: String = "Table availability for today"
     val textTomorrow: String = "Table availability for tomorrow"
 
-    comparison(allTables, availableTablesToday, textToday)
-    comparison(allTables, availableTablesTomorrow, textTomorrow)
+    comparison(occupiedTablesToday, textToday)
+    comparison(occupiedTablesTomorrow, textTomorrow)
 
 }
-fun comparison(allTables: Int, availableTables: Int, text: String){
-    if(availableTables == allTables){
+
+fun comparison(tables: Int, text: String) {
+    if (ALL_TABLES - tables == 0) {
         println("$text: ${false}")
-    } else{
+    } else {
         println("$text: ${true}")
     }
 }
