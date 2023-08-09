288e3e7d7c3c6914bfdc4c4a79c0d36144b058b1
FIsmailov <farhodbjj1984@gmail.com>
2023-08-09 14:17:09 +0500

    rename function, change logic

--- src/main/kotlin/available_restaurant_tables.kt
+++ src/main/kotlin/available_restaurant_tables.kt
@@ -1,19 +1,19 @@
 const val ALL_TABLES: Int = 13
 fun main() {
 
-    val occupiedTablesToday: Int = ALL_TABLES
+    val occupiedTablesToday: Int = 13
     val availableTablesTomorrow: Int = 4
     val occupiedTablesTomorrow: Int = ALL_TABLES - availableTablesTomorrow
     val textToday: String = "Table availability for today"
     val textTomorrow: String = "Table availability for tomorrow"
 
-    comparison(occupiedTablesToday, textToday)
-    comparison(occupiedTablesTomorrow, textTomorrow)
+    checkAvailabilityTable(occupiedTablesToday, textToday)
+    checkAvailabilityTable(occupiedTablesTomorrow, textTomorrow)
 
 }
 
-fun comparison(tables: Int, text: String) {
-    if (ALL_TABLES - tables == 0) {
+fun checkAvailabilityTable(tables: Int, text: String) {
+    if (tables == ALL_TABLES) {
         println("$text: ${false}")
     } else {
         println("$text: ${true}")
