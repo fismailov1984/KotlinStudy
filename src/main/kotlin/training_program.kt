4eae5db061acd035639976bb5ed4e7ba4c6c76b3
FIsmailov <farhodbjj1984@gmail.com>
2023-08-10 12:10:07 +0500

    rename variable

--- src/main/kotlin/training_program.kt
+++ src/main/kotlin/training_program.kt
@@ -2,6 +2,10 @@ fun main() {
 
     val trainingDay: Int = 5
     val exercise = "exercises of the"
+    val isEvent = when (trainingDay % 2) {
+        0 -> false
+        else -> true
+    }
 
     val mapMutableList = mutableMapOf(
         "arms" to 1,
@@ -11,6 +15,7 @@ fun main() {
     )
 
     for (i in mapMutableList) {
-        println("$exercise ${i.key}: ${(trainingDay % 2 == 1) && (i.value == 1) || (trainingDay % 2 == 0) && (i.value == 2)}")
+
+        println("$exercise ${i.key}: ${(isEvent) && (i.value == 1) || (!isEvent) && (i.value == 2)}")
     }
 }
